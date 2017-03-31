10115cc378aba8771cdc1e48744c8a5791e855b3
servercharlie <servercharlie@hotmail.com>
2017-03-31 22:27:12 +0800

    Updated module & test.

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+
+function KeyStore2() {
+	this.storage = {};
+	this.contexts = {};
+}
+
+KeyStore2.prototype.set = function (_key, _value) {
+	if (typeof _key === 'string') {
+		this.storage[_key] = _value;
+		return true;
 	}
-
-	opts = opts || {};
-
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	throw new Error('string only!');
 };
+KeyStore2.prototype.get = function (_key) {
+	if (typeof _key === 'string') {
+		return this.storage[_key];
+	}
+	throw new Error('string only!');
+};
+KeyStore2.prototype.clearContext = function (_context) {
+	this.contexts[_context].storage = {};
+	return true;
+};
+KeyStore2.prototype.pullContext = function (_context) {
+	if (typeof this.contexts[_context] === 'object') {
+		return this.contexts[_context];
+	}
+	this.contexts[_context] = new KeyStore2();
+	return this.contexts[_context];
+};
+
+if (typeof module !== 'undefined') {
+	module.exports = KeyStore2;
+}
--- test.js
+++ test.js
@@ -1,9 +1,43 @@
 import test from 'ava';
-import fn from '.';
+import KeyStore2 from '.';
 
-test('title', t => {
-	const err = t.throws(() => fn(123), TypeError);
-	t.is(err.message, 'Expected a string, got number');
+const _KeyStore = new KeyStore2();
 
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+test('set & get', t => {
+	_KeyStore.set('a', 1);
+
+	t.is(_KeyStore.get('a'), 1);
+});
+
+test('string keys only for set.', t => {
+	var hasError = false;
+	try {
+		_KeyStore.set([1, 2], 1);
+	} catch (err) {
+		hasError = true;
+	}
+	t.is(hasError, true);
+});
+
+test('string keys only for get.', t => {
+	var hasError = false;
+	try {
+		_KeyStore.get([1, 2]);
+	} catch (err) {
+		hasError = true;
+	}
+	t.is(hasError, true);
+});
+
+test('context set sample value', t => {
+	t.is(_KeyStore.pullContext('sample context').set('a', 1), true);
+});
+
+test('context get sample value', t => {
+	t.is(_KeyStore.pullContext('sample context').get('a'), 1);
+});
+
+test('context clear', t => {
+	_KeyStore.clearContext('sample context');
+	t.is(_KeyStore.pullContext('sample context').get('a'), undefined);
 });
